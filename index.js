3e4d14e6f9efe21d22409227a1c0e6a499d70141
alejandro21-rock <alejandrodeangelis21@gmail.com>
2025-03-31 13:26:43 -0300

    nuevo build

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -51,12 +53,12 @@ app.get("/api/notes/:id", (request, response, next) => {
 app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then((result) => {
-      response.status(204).end();
+      response.status(204).send(result);
     })
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -68,13 +70,16 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
-app.put("api/notes/:id", (request, response, next) => {
-  const body = request.body;
+/* app.put("api/notes/:id", (request, response, next) => {
+   const body = request.body; 
 
   const note = {
     content: body.content,
@@ -86,7 +91,23 @@ app.put("api/notes/:id", (request, response, next) => {
       response.json(updatedNote);
     })
     .catch((error) => next(error));
-});
+}); */
+
+app.put('/api/notes/:id', (request, response, next) => {
+
+  const { content, important } = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id, 
+
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });

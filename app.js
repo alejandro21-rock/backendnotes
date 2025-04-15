5d0acec3a653409c14bdc239c573683e3dad1e53
alejandro21-rock <alejandrodeangelis21@gmail.com>
2025-04-15 16:50:58 -0300

    autenticación basada en tokens

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require("express");
 require("express-async-errors");
 const app = express();
 const cors = require("cors");
+const loginRouter = require("./controllers/login");
 const notesRouter = require("./controllers/notes");
 const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
-const process = require('node:process')
+const process = require("node:process");
 /* mongoose.set("strictQuery", false);
 
 
@@ -41,6 +42,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use("/api/login", loginRouter);
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
 

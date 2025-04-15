9ff03fae97197273d20d44b1fb6a3f7712f7754a
alejandro21-rock <alejandrodeangelis21@gmail.com>
2025-04-15 13:01:01 -0300

    administración de usuarios

--- app.js
+++ app.js
@@ -4,21 +4,36 @@ require("express-async-errors");
 const app = express();
 const cors = require("cors");
 const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
+const process = require('node:process')
+/* mongoose.set("strictQuery", false);
 
-mongoose.set("strictQuery", false);
-
-logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
-  .connect(config.MONGODB_URI)
+.connect(config.MONGODB_URI)
+.then(() => {
+  logger.info("connected to MongoDB");
+})
+.catch((error) => {
+  logger.error("error connecting to MongoDB:", error.message);
+}); */
+
+logger.info("connecting to", config.MONGODB_URI);
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI;
+
+/* console.log("connecting to", url); */
+mongoose
+  .connect(url)
   .then(() => {
-    logger.info("connected to MongoDB");
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message);
+    console.log("error connecting to MongoDB:", error.message);
   });
 
 app.use(cors());
@@ -27,6 +42,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
